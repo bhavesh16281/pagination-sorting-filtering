e4189726c9b06b89925d9210cc690f8b3ec54aae
Muthyalu Bhavesh <66527078+bhavesh16281@users.noreply.github.com>
2026-08-15 21:08:02 +0530

    initial commit

---
+++ src/main/java/com/bhavesh16281/pagination_sorting_filtering/PaginationSortingFilteringApplication.java
@@ -0,0 +1,13 @@
+package com.bhavesh16281.pagination_sorting_filtering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationSortingFilteringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginationSortingFilteringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhavesh16281/pagination_sorting_filtering/PaginationSortingFilteringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhavesh16281.pagination_sorting_filtering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationSortingFilteringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
